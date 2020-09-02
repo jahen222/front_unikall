fb00635c96abb21690b2dad772bd62beac7fdcf9
mehsan1 <mehsan@thepointofit.com>
2020-09-02 23:13:08 +0500

    changes for font

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <div id="app">
+<div id="app">
     <router-view :key="$route.fullPath"></router-view>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: "App",
+    name: "App",
 };
 </script>
 
 <style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: 'Raleway', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
